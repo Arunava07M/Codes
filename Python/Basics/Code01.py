9c9b93dd5dc8526d388376eba50977615b988dc3
Arunava Mukherjee <arunavamukherjee016@gmail.com>
2026-04-13 22:16:28 +0530

    syntax

--- Python/Basics/Code01.py
+++ Python/Basics/Code01.py
@@ -3,3 +3,7 @@ print("hello world")
 # "hello world" is a string written in double quotes
 
 print("hello world", "with python")
+# comma is used to connect the two strings
+
+print("hello \n world")
+#\n space in output means how close to \n you are writing the string
