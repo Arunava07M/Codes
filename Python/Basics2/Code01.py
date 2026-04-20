3273c3318ea1ad62da9cac31cde1d61c89bf6397
Arunava Mukherjee <arunavamukherjee016@gmail.com>
2026-04-20 08:12:41 +0530

    syntax

---
+++ Python/Basics2/Code01.py
@@ -0,0 +1,3 @@
+word = "strings"
+
+print(len(word))
---
+++ Python/Basics2/Code02.py
@@ -0,0 +1,5 @@
+word1 = "I love"
+word2 = "python"
+
+print(word1 + " " + word2)#concatenate
+#you can do sentence = word1 + " " + word2 and print(sentence)
