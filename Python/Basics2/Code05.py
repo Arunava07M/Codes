317ba9be6c88f5e272c517add307335dbc0bc148
Arunava Mukherjee <arunavamukherjee016@gmail.com>
2026-04-20 08:50:38 +0530

    syntax

--- Python/Basics2/Code05.py
+++ Python/Basics2/Code05.py
@@ -2,4 +2,5 @@ word = "python"
 
 print(word[2:4])
 print(word[2:]) #assumes we want to go till the end of the string by default
-print(word[:len(word)])
+print(word[:len(word)])#0 to ending index
+print(word[:])# 0 to ending index
