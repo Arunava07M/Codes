c823704280fd48f2f47777df9ef271f584d7a443
Arunava Mukherjee <arunavamukherjee016@gmail.com>
2026-04-20 08:52:10 +0530

    syntax

--- Python/Basics2/Code05.py
+++ Python/Basics2/Code05.py
@@ -4,3 +4,4 @@ print(word[2:4])
 print(word[2:]) #assumes we want to go till the end of the string by default
 print(word[:len(word)])#0 to ending index
 print(word[:])# 0 to ending index
+print(word[-4:-2])
