a096c157f192ebfedf0c5a1547ef2e8111819a97
Arunava Mukherjee <arunavamukherjee016@gmail.com>
2026-04-24 15:06:01 +0530

    syntax

---
+++ Python/Basics2/Code20.py
@@ -0,0 +1,21 @@
+s = {1,2,3,4,5,6,7}
+
+s.add(8)
+
+print(s)
+
+s.remove(3)
+
+print(s)
+
+s.clear()
+
+print(s)
+
+s = {1,2,3,4}
+s1 = {5,6,7,8,9,10}
+s2 = {2,3,4,5,6,7}
+
+print(s.union(s1))
+
+print(s.intersection(s2))
